fd371a834c05cbd586185e8bbe102e2e15eff08c
PeterBarancok <peter.barancok@health.gov.sk>
2023-06-07 10:00:33 +0200

    Revert "Verzia pre data za rok 2022"
    This reverts commit ee7ff1ec4070118de189a35143fe3b79627404b9.

--- Skripty/06_prirad_sluzby.R
+++ Skripty/06_prirad_sluzby.R
@@ -4,81 +4,38 @@
 
 # Inicializuj premennu do ktorej sa budu ukladat hospitalizacie s priradenou
 # medicinskou sluzbou.
-uzs_ms_list <- list()
+uzs_list <- list()
 
-# Prirad sluzby podla toho co zohladnuju
+# Prirad sluzby postupne podla toho, co zohladnuju
 tmp_names <- names(ms_list)
 tmp_names <- tmp_names[tmp_names != "icd_vedlajsia"]
-for (n in tmp_names) {
-  print(paste("Priradujem sluzby podla: ", n))
-
-  # Ak sa zohladnuje viac ako jeden faktor, rozhod ich na vektor nazvov
-  if (n %like% ",") {
-    tmp_join_col <- unlist(strsplit(n, split = ", "))
-  } else {
-    tmp_join_col <- n
+for (i in tmp_names) {
+  name <- names(ms_list[i])
+  print(name)
+  if (name %like% ",") {
+    join_col <- unlist(strsplit(name, split = ", "))
+  } else{
+    join_col <- name
   }
-
-  # Sprav predvyber podla
-  #   premennych ktore nas zaujimaju
-  if ("icd_any" %in% tmp_join_col) {
-    tmp_data <- uzs_final %>%
-      select(id_hosp_nem, vek, HMOTNOST, UPV, typ_zs, vykon_lab,
-             any_of(tmp_join_col), icd, icd_vedlajsia) %>%
+  if ("icd_any" %in% join_col) {
+    uzs_list[[i]] <- rbind(uzs_final %>%
+                             inner_join(data.frame(ms_list[[i]]) %>% select(-icd, -icd_vedlajsia),
+                                        by = c(t(join_col[which(join_col != "icd_any")]),
+                                               "icd_vedlajsia" = "icd_any")),
+                           uzs_final %>%
+                             inner_join(data.frame(ms_list[[i]]) %>% select(-icd, -icd_vedlajsia),
+                                        by = c(t(join_col[which(join_col != "icd_any")]),
+                                               "icd" = "icd_any"))) %>%
       unique()
-  } else {
-    tmp_data <- uzs_final %>%
-      select(id_hosp_nem, vek, HMOTNOST, UPV, typ_zs, vykon_lab,
-             all_of(tmp_join_col)) %>%
-      unique()
+  }else if (c("icd") != join_col) {
+    uzs_list[[i]] <- uzs_final %>% inner_join(data.frame(ms_list[[i]]), by = t(join_col)) %>% unique()
+  } else{
+    temp <- data.table(uzs_final) %>% unique()
+    uzs_list[[i]] <- temp %>% inner_join(data.frame(ms_list[[i]]), by = t(join_col)) %>% unique()
+    remove(temp)
+    gc()
   }
-  #   DRG
-  if ("drg" %in% tmp_join_col) {
-    tmp_data <- tmp_data %>%
-      filter(drg %in% ms_drg)
-  }
-  #   vykonu
-  if ("vykon" %in% tmp_join_col) {
-    tmp_data <- tmp_data %>%
-      filter(vykon != "")
-  }
-  #   diagnozy
-  if ("icd" %in% tmp_join_col) {
-    tmp_data <- tmp_data %>%
-      filter(icd %in% ms_diag)
-  }
-  if ("icd_vedlajsia" %in% tmp_join_col) {
-    tmp_data <- tmp_data %>%
-      filter(icd_vedlajsia %in% ms_diag)
-  }
-  if ("icd_any" %in% tmp_join_col) {
-    tmp_data <- tmp_data %>%
-      filter((icd_vedlajsia %in% ms_diag) | (icd %in% ms_diag))
-  }
-
-  # Prirad sluzby. Ak je tam "icd_any", tak treba priradenie spravit dvakrat,
-  # raz pre "icd" a raz pre "icd_vedlajsia"
-  if ("icd_any" %in% tmp_join_col) {
-    uzs_ms_list[[n]] <- rbind(
-      tmp_data %>%
-        inner_join(data.frame(ms_list[[n]]) %>%
-                     select(-icd, -icd_vedlajsia),
-                   by = c(t(tmp_join_col[which(tmp_join_col != "icd_any")]),
-                          "icd_vedlajsia" = "icd_any")),
-      tmp_data %>%
-        inner_join(data.frame(ms_list[[n]]) %>%
-                     select(-icd, -icd_vedlajsia),
-                   by = c(t(tmp_join_col[which(tmp_join_col != "icd_any")]),
-                          "icd" = "icd_any"))
-    ) %>%
-      unique()
-  } else {
-    uzs_ms_list[[n]] <- tmp_data %>%
-      inner_join(data.frame(ms_list[[n]]), by = t(tmp_join_col)) %>%
-      unique()
-  }
-
-  invisible(gc())
+  gc()
 }
 
 # Vymaz docasne premenne a uvolni pamat
